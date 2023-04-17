01e01b94caa6163315a6c2d499f3d714ff1d29be
Marlon Vinicius de Souza <marlon.vinicius2@gmail.com>
2023-04-17 20:12:19 -0300

    (17/04) Criacao métodos POST,PUT,DELETE

--- Aula_06/index.js
+++ Aula_06/index.js
@@ -6,18 +6,48 @@ app.use(express.json())
 
 app.listen(8080,() => {
     console.log("O servidor está ativo na porta 8080");
-})
-
-// CRUD
-// GET POST PUT DELETE
-app.get('/', () => {
-    console.log('O NEGÓCIO DEU CERTO!!!');
 });
 
-//SELECT * FROM Alunos WHERE id = 123
-app.get('/getAluno', (req, res) => {
-    const { id } = req.body;
-    console.log(`O aluno de ID: ${id} foi encontrado`);
-    res.send(`O aluno de ID: ${id} foi encontrado`);
+let Alunos = ['Maria', 'João', 'José'];
 
-})
+// CRUD - GET POST PUT DELETE
+
+// CREATE
+app.post('/includeAluno', (req, res) => {
+    const { nome } = req.body
+    Alunos.push(nome);
+    const { idade } = req.body
+
+    res.send(`<h1> Olá ${nome}, seja bem vindo à sua primeira API! Você tem ${idade} anos de idade.`)
+});
+
+// READ
+app.get('/getAluno', (req, res) => {
+    const { index } = req.body;
+    // connect SQL -
+    // Select * From Alunos WHERE id = index
+    res.send(`<h1>O aluno ${Alunos[index]} foi encontrado</h1>`);
+});
+
+app.get('/getAlunos', (req, res) => {
+    // SELECT * From Alunos
+    console.log(Alunos);
+    res.send(`Todos os alunos cadastrados são: ${Alunos}`)
+});
+
+// UPDATE
+app.put('/updateAluno', (req,res) => {
+    // UPDATE nome FROM Alunos WHERE id = index;
+    const {index,nome} = req.body;
+    Alunos[index] = nome;
+    res.send(`<h1>O nome foi atualizado com sucesso!!! </h1>`);
+    console.log(Alunos);
+});
+
+// DELETE
+app.delete('/deleteAluno', (req,res) => {
+    // DELETE FROM Alunos WHERE id = index
+    const {index} = req.body;
+    Alunos.splice(index, 1);
+    res.send(`<h1>Alunos após o DELETE: ${Alunos}`);
+});
