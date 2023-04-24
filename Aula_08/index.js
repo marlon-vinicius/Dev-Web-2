1cd576dc3025f21249af4f03004679890ce41e07
Marlon Vinicius de Souza <marlon.vinicius2@gmail.com>
2023-04-24 20:41:49 -0300

    (24/04)Trabalhando com Postgres

---
+++ Aula_08/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { pool } = require("./data/data");
+const app = express();
+app.use(express.json());
+app.listen(8080, () => {
+    console.log("O servidor está ativo na porta 8080!!!");
+});
+
+pool.connect();
+
+app.get("/getUsers", async (req,res) => {
+    try {
+        const client = await pool.connect();
+        const { rows } = await client.query
+        ("SELECT * From Users");
+        console.table(rows);
+        res.status(200).send(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Erro de conexão com o servidor");
+    }
+})
